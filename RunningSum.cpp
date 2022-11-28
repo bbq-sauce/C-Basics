7eaa5eaa19720ac58872b7c0868e70c78d60e57b
Fiza <73010228+bbq-sauce@users.noreply.github.com>
2022-11-28 22:32:32 +0530

    runningsumcode

---
+++ RunningSum.cpp
@@ -0,0 +1,37 @@
+//code to get the running sum of elements in an array.
+//eg- input: 1 2 3 4
+//output: 1 3 6 10 
+#include <iostream>
+using namespace std;
+
+void runningsum(int arr[],int n)
+{
+    int temp= arr[0];
+    for(int i=1;i<n;i++)
+    {
+        arr[i]=temp+arr[i];
+        temp = arr[i];
+    }
+    
+    
+    
+}
+
+int main()
+{
+    int arr[5];
+    
+    //enter array
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+        
+    }
+    runningsum(arr,5);
+    
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
