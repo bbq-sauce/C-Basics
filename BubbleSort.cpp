6ecace69e2bd410780f38be0c532eaec729b54dc
Fiza <73010228+bbq-sauce@users.noreply.github.com>
2022-12-02 15:11:49 +0530

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int binsearch(int arr[],int n){
+  
+  int i,j;
+  for(i=0;i<n-1;i++)
+    for(j=0;j<n-i-1;j++)
+      if(arr[j]>arr[j+1])
+      {swap(arr[j],arr[j+1]);}
+  return -1;
+}
+
+
+void printarray(int ar[],int n){
+  for(int i=0;i<n;i++)
+    std::cout<<ar[i]<<" ";
+
+  std::cout<<endl;
+}
+int main() {
+  std::cout << "Hello World!\n";
+  int arr[]={4,2,6,1,3};
+  int n = sizeof(arr)/sizeof(arr[0]);
+
+  binsearch(arr,  n);
+  std::cout<<"sorted array:"<<endl;
+  printarray(arr,  n);
+  return 0;
+  
+}
